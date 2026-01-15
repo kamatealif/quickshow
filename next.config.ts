a70e4eedb6bd51df5b254996a27b81921c688e8f
kamatealif <kamatealip1@gmail.com>
2026-01-15 18:19:19 +0530

    fixed next.config.ts to support tmdb images

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    domains:['image.tmdb.org']
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
